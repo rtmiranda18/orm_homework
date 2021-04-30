4e2d0a6c2ed34a2cdb7f57a03bbe5ec7cacb10f6
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-30 10:53:12 +0800

    input relations in index.js under models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+  as: 'product_options'
+});
 
 // Categories have many Products
+Category.haveMany(Product, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+  as: 'category_choice'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_progress'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_options'
+});
 
 module.exports = {
   Product,
